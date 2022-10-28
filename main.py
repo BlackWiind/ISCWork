566cb03936aa5e0feeb159c8bc7d5c2460feca73
BlackWiind <BlackWiind@gmail.com>
2022-10-28 15:45:26 +1000

    Функции нахождения H и ~H матриц. Изменение в RSA алгоритме, теперь может сам находить d.

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import random
 import rsa_alg
+from common_algorithms import module_degree
 
 CONSTANT_P: int = 19
 CONSTANT_Q: int = 41
+VARIANT: int = 3
 BASE_MATRIX: list = [
     [0, 0, 1, 0, 0, 1, 1, 1],
     [0, 0, 1, 1, 0, 0, 0, 1],
@@ -14,18 +17,46 @@ BASE_MATRIX: list = [
 ]
 
 
+def izomorf(rule: int) -> list:
+    """
+    Находит изоморфный граф.
+    Возвращает матрицу смежности.
+    :param rule: int
+    :return: List[][]
+    """
+    rules_of_nature: list = []
+    for row in range(len(BASE_MATRIX)):
+        # ((a+Z)mod 8), изменино с 9 на 8 т.к. индексация с нуля
+        rules_of_nature.append(module_degree((row + rule), 1, 8))
+
+    new_matrix = [[0] * len(BASE_MATRIX) for _ in range(len(BASE_MATRIX))]
+    for row_index, row in enumerate(BASE_MATRIX):
+        for item_index, item in enumerate(row):
+            new_matrix[row_index][item_index] = BASE_MATRIX[rules_of_nature[item_index]][rules_of_nature[row_index]]
+
+    return new_matrix
+
+
+def tilda_matrix(matrix: list) -> list:
+    """
+    Приписывает к значениям в матрице случайные числа слева.
+    Технически плюсует десятки.
+    :param matrix: List[][]
+    :return: List[][]
+    """
+    tuple_for_random: tuple = (10, 20, 30, 40, 50)
+    for row_index, row in enumerate(matrix):
+        for item_index, item in enumerate(row):
+            matrix[row_index][item_index] += random.choice(tuple_for_random)
+    return matrix
+
+
 def main():
-    indexs = [6, 3, 4, 2, 0, 1, 7, 5]
-    new_matrix = BASE_MATRIX
-    for i in range(8):
-        for j in range(8):
-            print(i,j)
-            new_matrix[i][j] = BASE_MATRIX[indexs[i]][indexs[j]]
-            print(indexs[i], indexs[j])
-            print(BASE_MATRIX[indexs[i]][indexs[j]])
-        print(new_matrix[i])
-    for matrix in new_matrix:
-        print(f"{matrix}\n")
+    h_matrix = izomorf(VARIANT)
+    h_matrix = tilda_matrix(h_matrix)
+
+    for matrix_row in h_matrix:
+        print(f"{matrix_row}")
 
 
 if __name__ == '__main__':
--- rsa_alg.py
+++ rsa_alg.py
@@ -1,16 +1,21 @@
 # RSA шифр, в качестве сообщения только целые числа
+import random
+
 from common_algorithms import module_degree, evclid_extended
 
 
 class RSAAlg:
 
-    def __init__(self, p: int, q: int, d: int):
+    def __init__(self, p: int, q: int, d: int = None):
         self.n = p * q
-        self.d = d
         self.fi = (p - 1) * (q - 1)
+        self.d = d if d else find_d(self.fi)
         self.open_key = (d, self.n)
         private_key = evclid_extended(self.fi, self.d)[2]
         self.private_key = private_key if private_key > 0 else private_key + self.fi
+
+    def find_d(self):
+        pass
 
     def encrypt_message(self, open_key: tuple, message: int) -> int:
         # Шифровка сообщения, open key от адресата сообщения
@@ -19,6 +24,13 @@ class RSAAlg:
     def decrypt_message(self, encrypted_message: int) -> int:
         # Расшифровка сообщения
         return module_degree(encrypted_message, self.private_key, self.n)
+
+
+@staticmethod
+def find_d(fi: int) -> int:
+    # Находит d взаимно простое с fi такое, что d<fi
+    d: int = random.randint(fi-1)
+    return d if evclid_extended(d, fi)[0] == 1 else find_d(fi)
 
 
 def demonstration(p: int, q: int, d: int, message: int):
